70267cd63200c985f9aa4fbcc4490cab1516ad8c
hiromaily <hiromaily2@gmail.com>
2018-08-14 17:55:08 +0900

    tweak

--- pkg/api/account.go
+++ pkg/api/account.go
@@ -1,3 +1,22 @@
 package api
 
-//test用にtestアカウントを作成するのはいいかもしれない。
+import (
+	"github.com/pkg/errors"
+)
+
+//TODO:test用にtestアカウントを作成するのはいいかもしれない。
+
+// GetAccount 渡されたアドレスから該当するアカウント名を取得する
+func (b *Bitcoin) GetAccount(addr string) (string, error) {
+	address, err := b.DecodeAddress(addr)
+	if err != nil {
+		return "", errors.Errorf("DecodeAddress(%s): error: %v", addr, err)
+	}
+
+	accountName, err := b.client.GetAccount(address)
+	if err != nil {
+		return "", errors.Errorf("client.GetAccount(): error: %v", err)
+	}
+
+	return accountName, nil
+}
--- pkg/api/address.go
+++ pkg/api/address.go
@@ -6,12 +6,26 @@ import (
 )
 
 // CreateNewAddress アカウント名から新しいアドレスを生成する
+// 常に新しいアドレスが生成される
 // これによって作成されたアカウントはbitcoin core側のwalletで管理される
 // TODO:おそらく本番では使わない
 func (b *Bitcoin) CreateNewAddress(accountName string) (btcutil.Address, error) {
 	addr, err := b.client.GetNewAddress(accountName)
 	if err != nil {
-		return nil, errors.Errorf("GetNewAddress(%s): error: %v", accountName, err)
+		return nil, errors.Errorf("client.GetNewAddress(%s): error: %v", accountName, err)
+	}
+
+	return addr, nil
+}
+
+// GetAccountAddress アカウントに紐づくアドレスを返す
+// => アカウントに紐づくアドレスが無い場合は新規アドレスを作成し、そのアドレス値を返す
+// => アカウントに紐づくアドレスが既にある場合は新規作成せずに既存アドレス値を返す。
+// => 既存アドレスがある場合でもそれが使用済み（一度以上BTCを受け取った）の場合には新規アドレスを作成し、そのアドレス値を返す。
+func (b *Bitcoin) GetAccountAddress(accountName string) (btcutil.Address, error) {
+	addr, err := b.client.GetAccountAddress(accountName)
+	if err != nil {
+		return nil, errors.Errorf("client.GetAccountAddress(%s): error: %v", accountName, err)
 	}
 
 	return addr, nil
@@ -20,9 +34,16 @@ func (b *Bitcoin) CreateNewAddress(accountName string) (btcutil.Address, error) 
 // GetAddressesByAccount アカウント名から紐づくすべてのアドレスを取得する
 // TODO:本番でaddressに対してアカウント名を紐付けることをするかどうか未定
 func (b *Bitcoin) GetAddressesByAccount(accountName string) ([]btcutil.Address, error) {
+	//Returns the list of addresses for the given account.
+	//アカウント名に紐づくアドレス一覧
+	//[
+	//	"2MvqroV1E2FjrRaWjQuLcfotZabhkSuYNGi",
+	//	"2NGVf4fHRkWCtUBw4VXCtBMJdZBdgL7ffAq"
+	//]
+
 	addrs, err := b.client.GetAddressesByAccount(accountName)
 	if err != nil {
-		return nil, errors.Errorf("GetAddressesByAccount(%s): error: %v", accountName, err)
+		return nil, errors.Errorf("client.GetAddressesByAccount(%s): error: %v", accountName, err)
 	}
 
 	return addrs, nil
