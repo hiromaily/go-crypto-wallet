0827c85660fd62fadab22bba0005d7e197819fed
HirokiYasui <hiromaily2@gmail.com>
2020-04-17 17:58:32 +0900

    refactoring create_tx

--- pkg/wallet/wallets/wallet/create_tx_receipt.go
+++ pkg/wallet/wallets/wallet/create_tx_receipt.go
@@ -1,151 +1,20 @@
 package wallet
 
 import (
-	"fmt"
-
-	"github.com/bookerzzz/grok"
-	"github.com/btcsuite/btcd/btcjson"
-	"github.com/btcsuite/btcutil"
-	"github.com/pkg/errors"
-	"go.uber.org/zap"
-
 	"github.com/hiromaily/go-bitcoin/pkg/account"
 	"github.com/hiromaily/go-bitcoin/pkg/action"
-	"github.com/hiromaily/go-bitcoin/pkg/model/rdb/walletrepo"
-	"github.com/hiromaily/go-bitcoin/pkg/wallet/api/btc"
 )
 
-// - check unspentTx for client
-// - get utxo and create unsigned tx
-// - fee is fluctuating, so outdated unsigned tx must not be used, re-create unsigned tx
-// - after signed tx sent, utxo could not be retrieved by listUnspent()
-
-type parsedTx struct {
-	txInputs       []btcjson.TransactionInput
-	txRepoTxInputs []walletrepo.TxInput
-	prevTxs        []btc.PrevTx
-	addresses      []string
-}
-
 // CreateReceiptTx create unsigned tx if client accounts have coins
-// - sender account: client, receiver account: receipt
-// - receiver account covers fee
 func (w *Wallet) CreateReceiptTx(adjustmentFee float64) (string, string, error) {
-	fixedAccount := account.AccountTypeClient
-
-	// get listUnspent
-	unspentList, err := w.getUnspentList(fixedAccount)
-	if len(unspentList) == 0 {
-		w.logger.Info("no listunspent")
-		return "", "", nil
-	}
-
-	// parse listUnspent
-	parsedTx, inputTotal := w.parseListUnspentTx(unspentList)
-	w.logger.Debug(
-		"total coin to send (Satoshi) before fee calculated",
-		zap.Any("input_amount", inputTotal),
-		zap.Int("len(inputs)", len(parsedTx.txInputs)))
-	if len(parsedTx.txInputs) == 0 {
-		return "", "", nil
-	}
-
-	addrsPrevs := btc.AddrsPrevTxs{
-		Addrs:         parsedTx.addresses,
-		PrevTxs:       parsedTx.prevTxs,
-		SenderAccount: fixedAccount,
-	}
-
-	//TODO: how this code can be integrated with CreateTransferTx ??
-
-	// create raw tx
-	hex, fileName, err := w.createRawTx(
-		action.ActionTypeReceipt,
-		account.AccountTypeReceipt,
-		adjustmentFee,
-		parsedTx.txInputs,
-		inputTotal,
-		parsedTx.txRepoTxInputs,
-		&addrsPrevs)
-
-	//TODO: notify what unsigned tx is created
-	// => in command pkg
-
-	return hex, fileName, err
-}
-
-// call API `unspentlist`
-// no result and no error is possible, so caller should check both returned value
-func (w *Wallet) getUnspentList(accountType account.AccountType) ([]btc.ListUnspentResult, error) {
-	// unlock locked UnspentTransaction
-	//if err := w.BTC.UnlockAllUnspentTransaction(); err != nil {
-	//	return "", "", err
-	//}
-
-	// get listUnspent
-	unspentList, _, err := w.btc.ListUnspentByAccount(accountType)
+	sender := account.AccountTypeClient
+	receiver := account.AccountTypeReceipt
+	targetAction := action.ActionTypeReceipt
+	amount, err := w.btc.FloatBitToAmount(0)
 	if err != nil {
-		return nil, errors.Wrap(err, "fail to call btc.Client().ListUnspent()")
+		return "", "", err
 	}
-	grok.Value(unspentList)
-	if len(unspentList) == 0 {
-		w.logger.Info("no listunspent")
-		return nil, nil
-	}
-	return unspentList, nil
-}
 
-// parse result of listUnspent
-// retured *parsedTx could be nil
-func (w *Wallet) parseListUnspentTx(unspentList []btc.ListUnspentResult) (*parsedTx, btcutil.Amount) {
-	var inputTotal btcutil.Amount
-	txInputs := make([]btcjson.TransactionInput, 0, len(unspentList))
-	txRepoTxInputs := make([]walletrepo.TxInput, 0, len(unspentList))
-	prevTxs := make([]btc.PrevTx, 0, len(unspentList))
-	addresses := make([]string, 0, len(unspentList))
-
-	for _, tx := range unspentList {
-		// Amount
-		amt, err := btcutil.NewAmount(tx.Amount)
-		if err != nil {
-			//this error is not expected
-			w.logger.Error(
-				"btcutil.NewAmount()",
-				zap.Float64("tx amount", tx.Amount),
-				zap.Error(err))
-			continue
-		}
-		inputTotal += amt
-
-		txInputs = append(txInputs, btcjson.TransactionInput{
-			Txid: tx.TxID,
-			Vout: tx.Vout,
-		})
-
-		txRepoTxInputs = append(txRepoTxInputs, walletrepo.TxInput{
-			ReceiptID:          0,
-			InputTxid:          tx.TxID,
-			InputVout:          tx.Vout,
-			InputAddress:       tx.Address,
-			InputAccount:       tx.Label,
-			InputAmount:        fmt.Sprintf("%f", tx.Amount),
-			InputConfirmations: tx.Confirmations,
-		})
-
-		prevTxs = append(prevTxs, btc.PrevTx{
-			Txid:         tx.TxID,
-			Vout:         tx.Vout,
-			ScriptPubKey: tx.ScriptPubKey,
-			RedeemScript: "", //required if target account is multsig address
-			Amount:       tx.Amount,
-		})
-
-		addresses = append(addresses, tx.Address)
-	}
-	return &parsedTx{
-		txInputs:       txInputs,
-		txRepoTxInputs: txRepoTxInputs,
-		prevTxs:        prevTxs,
-		addresses:      addresses,
-	}, inputTotal
+	// create receipt transaction
+	return w.createTx(sender, receiver, targetAction, amount, adjustmentFee)
 }
--- pkg/wallet/wallets/wallet/create_tx_transfer.go
+++ pkg/wallet/wallets/wallet/create_tx_transfer.go
@@ -1,17 +1,9 @@
 package wallet
 
 import (
-	"fmt"
-
-	"github.com/btcsuite/btcd/btcjson"
-	"github.com/btcsuite/btcutil"
-	"github.com/pkg/errors"
-	"go.uber.org/zap"
-
 	"github.com/hiromaily/go-bitcoin/pkg/account"
 	"github.com/hiromaily/go-bitcoin/pkg/action"
-	"github.com/hiromaily/go-bitcoin/pkg/model/rdb/walletrepo"
-	"github.com/hiromaily/go-bitcoin/pkg/wallet/api/btc"
+	"github.com/pkg/errors"
 )
 
 // CreateTransferTx transfer coin among internal account except client, authorization
@@ -19,6 +11,7 @@ import (
 // TODO: temporary fixed account is used `receipt to payment`
 // TODO: after this func, what if `listtransactions` api is called to see result
 func (w *Wallet) CreateTransferTx(sender, receiver account.AccountType, floatAmount, adjustmentFee float64) (string, string, error) {
+	targetAction := action.ActionTypeTransfer
 
 	// validation
 	if receiver == account.AccountTypeClient || receiver == account.AccountTypeAuthorization {
@@ -27,6 +20,7 @@ func (w *Wallet) CreateTransferTx(sender, receiver account.AccountType, floatAmo
 	if sender == receiver {
 		return "", "", errors.New("invalid account. sender and receiver is same")
 	}
+
 	//amount btcutil.Amount
 	amount, err := w.btc.FloatBitToAmount(floatAmount)
 	if err != nil {
@@ -43,102 +37,6 @@ func (w *Wallet) CreateTransferTx(sender, receiver account.AccountType, floatAmo
 		return "", "", errors.Errorf("account: %s balance is insufficient", sender)
 	}
 
-	//指定金額になるまで、utxoからinputを作成する
-	// Listunspent()にてpaymentアカウント用のutxoをすべて取得する
-	unspentList, _, err := w.btc.ListUnspentByAccount(sender)
-	if err != nil {
-		return "", "", errors.Errorf("BTC.ListUnspentByAccount(%s) error: %s", sender, err)
-	}
-
-	if len(unspentList) == 0 {
-		w.logger.Info("no listunspent for from account", zap.String("account", sender.String()))
-		return "", "", nil
-	}
-
-	//送金先は1つのアドレスなので、receipt.goのロジックに近い
-	var (
-		inputs          []btcjson.TransactionInput
-		inputTotal      btcutil.Amount
-		txReceiptInputs []walletrepo.TxInput
-		prevTxs         []btc.PrevTx
-		addresses       []string
-	)
-
-	for _, tx := range unspentList {
-
-		// Amount
-		amt, err := btcutil.NewAmount(tx.Amount)
-		if err != nil {
-			//このエラーは起こりえない
-			w.logger.Error(
-				"btcutil.NewAmount()",
-				zap.Float64("amount", tx.Amount),
-				zap.Error(err))
-			continue
-		}
-		inputTotal += amt //合計
-
-		//TODO:Ver17対応が必要
-		//lockunspentによって、該当トランザクションをロックして再度ListUnspent()で出力されることを防ぐ
-		//if w.BTC.LockUnspent(tx) != nil {
-		//	continue
-		//}
-
-		// inputs
-		inputs = append(inputs, btcjson.TransactionInput{
-			Txid: tx.TxID,
-			Vout: tx.Vout,
-		})
-
-		// txReceiptInputs
-		txReceiptInputs = append(txReceiptInputs, walletrepo.TxInput{
-			ReceiptID:          0,
-			InputTxid:          tx.TxID,
-			InputVout:          tx.Vout,
-			InputAddress:       tx.Address,
-			InputAccount:       tx.Label,
-			InputAmount:        fmt.Sprintf("%f", tx.Amount),
-			InputConfirmations: tx.Confirmations,
-		})
-
-		// prevTxs(walletでの署名でもversion17からは必要になる。。。fuck)
-		prevTxs = append(prevTxs, btc.PrevTx{
-			Txid:         tx.TxID,
-			Vout:         tx.Vout,
-			ScriptPubKey: tx.ScriptPubKey,
-			RedeemScript: tx.RedeemScript,
-			Amount:       tx.Amount,
-		})
-
-		//tx.Address
-		addresses = append(addresses, tx.Address)
-
-		//totalをチェック
-		if amount == 0 {
-			continue
-		}
-		if inputTotal > amount {
-			break
-		}
-	}
-
-	w.logger.Debug(
-		"total coin to send (Satoshi) before fee calculated, input length: %d",
-		zap.Any("amount", inputTotal),
-		zap.Int("len(inputs)", len(inputs)))
-	if len(inputs) == 0 {
-		return "", "", nil
-	}
-
-	addrsPrevs := btc.AddrsPrevTxs{
-		Addrs:         addresses,
-		PrevTxs:       prevTxs,
-		SenderAccount: sender,
-	}
-
-	// 一連の処理を実行
-	hex, fileName, err := w.createRawTx(action.ActionTypeTransfer, receiver, 0, inputs,
-		inputTotal, txReceiptInputs, &addrsPrevs)
-
-	return hex, fileName, err
+	// create transfer transaction
+	return w.createTx(sender, receiver, targetAction, amount, adjustmentFee)
 }
