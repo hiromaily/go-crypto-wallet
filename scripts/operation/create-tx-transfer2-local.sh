90049eacfa80e42bc89b491c3382cc8bf1ae149d
HirokiYasui <hiromaily2@gmail.com>
2020-04-20 14:49:52 +0900

    add

---
+++ scripts/operation/create-tx-transfer2-local.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# prepare coin for client address
+# https://coinfaucet.eu/en/btc-testnet/
+
+set -eu
+
+# create unsigned tx
+echo 'create transfer tx'
+tx_file=$(wallet create transfer -account1 receipt -account2 payment)
+if [ "`echo $tx_file | grep 'No utxo'`" ]; then
+  echo 'No utxo'
+  exit 0
+fi
+
+# sign on keygen wallet for 1st signature
+echo 'sign on 1st '${tx_file##*\[fileName\]: }
+tx_file_signed=`keygen sign -file "${tx_file##*\[fileName\]: }"`
+
+# sign on sign wallet for 2nd signature
+echo 'sign on 2nd '${tx_file_signed##*\[fileName\]: }
+tx_file_signed2=`sign sign -file "${tx_file_signed##*\[fileName\]: }"`
+
+# send signed tx
+echo 'send tx '${tx_file_signed2##*\[fileName\]: }
+tx_id=`wallet send -file "${tx_file_signed2##*\[fileName\]: }"`
+echo 'txID:'${tx_id##*txID: }
