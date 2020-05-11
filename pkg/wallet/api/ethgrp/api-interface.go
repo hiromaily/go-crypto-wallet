a27225c5746459814081f3cbef0e07338bfce856
HirokiYasui <hiromaily2@gmail.com>
2020-05-11 22:52:51 +0900

    define interface for eth

--- pkg/wallet/api/ethgrp/api-interface.go
+++ pkg/wallet/api/ethgrp/api-interface.go
@@ -2,9 +2,10 @@ package ethgrp
 
 import (
 	"crypto/ecdsa"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/ethereum/go-ethereum"
 	"math/big"
 
-	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/p2p"
 
@@ -14,23 +15,72 @@ import (
 
 // Ethereumer Ethereum Interface
 type Ethereumer interface {
-	// admin
-	NodeInfo() (*p2p.NodeInfo, error)
 	// client
 	BalanceAt(hexAddr string) (*big.Int, error)
 	SendRawTx(tx *types.Transaction) error
-	// eth
-	Syncing() (*eth.ResponseSyncing, bool, error)
 	// ethereum
 	Close()
 	GetChainConf() *chaincfg.Params
 	// key
 	ToECDSA(privKey string) (*ecdsa.PrivateKey, error)
 	GetKeyDir(accountType account.AccountType) string
-	// net
+
+	// rpc_admin
+	AddPeer(nodeURL string) error
+	AdminDataDir() (string, error)
+	NodeInfo() (*p2p.NodeInfo, error)
+	AdminPeers() ([]*p2p.PeerInfo, error)
+	// rpc_eth
+	Syncing() (*eth.ResponseSyncing, bool, error)
+	ProtocolVersion() (uint64, error)
+	Coinbase() (string, error)
+	Accounts() ([]string, error)
+	BlockNumber() (*big.Int, error)
+	GetBalance(hexAddr string, quantityTag eth.QuantityTag) (*big.Int, error)
+	GetStoreageAt(hexAddr string, quantityTag eth.QuantityTag) (string, error)
+	GetTransactionCount(hexAddr string, quantityTag eth.QuantityTag) (*big.Int, error)
+	GetBlockTransactionCountByHash(txHash string) (*big.Int, error)
+	GetBlockTransactionCountByNumber(number uint64) (*big.Int, error)
+	GetUncleCountByBlockHash(blockHash string) (*big.Int, error)
+	GetUncleCountByBlockNumber(blockNumber uint64) (*big.Int, error)
+	GetCode(hexAddr string, quantityTag eth.QuantityTag) (*big.Int, error)
+	GetBlockByNumber(quantityTag eth.QuantityTag) (*big.Int, error)
+	// rpc_eth_gas
+	GasPrice() (*big.Int, error)
+	EstimateGas(msg ethereum.CallMsg) (*big.Int, error)
+	// rpc_eth_tx
+	Sign(hexAddr, message string) (string, error)
+	SendTransaction(msg ethereum.CallMsg) (string, error)
+	SendRawTransaction(signedTx string) (string, error)
+	SendRawTransactionWithTypesTx(tx *types.Transaction) (string, error)
+	GetTransactionByHash(hashTx string) (*eth.ResponseGetTransaction, error)
+	GetTransactionReceipt(hashTx string) (*eth.ResponseGetTransactionReceipt, error)
+	// rpc_miner
+	StartMining() error
+	StopMining() error
+	Mining() (bool, error)
+	HashRate() (*big.Int, error)
+	// rpc_net
 	NetVersion() (uint16, error)
-	// personal
+	NetListening() (bool, error)
+	NetPeerCount() (*big.Int, error)
+	// rpc_personal
 	ImportRawKey(hexKey, passPhrase string) (string, error)
-	// web3
+	ListAccounts() ([]string, error)
+	NewAccount(passphrase string, accountType account.AccountType) (string, error)
+	LockAccount(hexAddr string) error
+	UnlockAccount(hexAddr, passphrase string, duration uint64) (bool, error)
+	// rpc_web3
 	ClientVersion() (string, error)
+	SHA3(data []string) (string, error)
+	// transaction
+	CreateRawTransaction(fromAddr, toAddr string, amount uint64) (string, []byte, error)
+	SignOnRawTransaction(rawTx *eth.RawTx, passphrase string, accountType account.AccountType) (*eth.RawTx, []byte, error)
+	SendSignedRawTransaction(signedTxHex string) (string, error)
+	// util
+	DecodeBig(input string) (*big.Int, error)
+	ValidationAddr(addr string) error
+	FromWei(v int64) *big.Int
+	FromGWei(v int64) *big.Int
+	FromEther(v int64) *big.Int
 }
