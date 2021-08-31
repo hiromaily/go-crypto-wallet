300bb34ee28d84ffca96749f07b6ff726fa1c6ee
HirokiYasui <hiromaily2@gmail.com>
2021-08-31 19:47:28 +0900

    add erc20 const

---
+++ pkg/wallet/coin/erc20.go
@@ -0,0 +1,27 @@
+package coin
+
+// ERC20Token erc20 token
+type ERC20Token string
+
+// erc20_token
+const (
+	BAT ERC20Token = "bat"
+)
+
+// String converter
+func (e ERC20Token) String() string {
+	return string(e)
+}
+
+// ERC20Address
+var ERC20Address = map[ERC20Token]string{
+	BAT: "0x0D8775F648430679A709E98d2b0Cb6250d2887EF",
+}
+
+// ValidateERC20Code validate
+func ValidateERC20Code(val string) bool {
+	if _, ok := ERC20Address[ERC20Token(val)]; ok {
+		return true
+	}
+	return false
+}
