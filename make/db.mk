77fcf124befb4f13c2d05a68e5e37e311740e072
HirokiYasui <hiromaily2@gmail.com>
2025-12-31 20:25:28 +0900

    Refactor sqlc settings (#191)
    * add db_sqlc.mk
    
    * add scritps
    
    * add scritps

--- make/db.mk
+++ make/db.mk
@@ -14,33 +14,9 @@ reset-docker-db:
 
 
 ###############################################################################
-# Schema Export Targets
+# DB Related Targets
 ###############################################################################
-
-# Export watch schema from wallet-db container
-.PHONY: dump-schema-watch
-dump-schema-watch:
-	mkdir -p data/dump/sql
-	docker exec wallet-db mysqldump -u root -proot --no-data --skip-triggers watch > data/dump/sql/dump_watch.sql
-
-# Export keygen schema from wallet-db container
-.PHONY: dump-schema-keygen
-dump-schema-keygen:
-	mkdir -p data/dump/sql
-	docker exec wallet-db mysqldump -u root -proot --no-data --skip-triggers keygen > data/dump/sql/dump_keygen.sql
-
-# Export sign schema from wallet-db container
-.PHONY: dump-schema-sign
-dump-schema-sign:
-	mkdir -p data/dump/sql
-	docker exec wallet-db mysqldump -u root -proot --no-data --skip-triggers sign > data/dump/sql/dump_sign.sql
-
-# Export all schemas from wallet-db container
-.PHONY: dump-schema-all
-dump-schema-all: dump-schema-watch dump-schema-keygen dump-schema-sign
-
-###############################################################################
-# Atlas Migration Targets
-###############################################################################
-# Atlas-related targets are defined in make/db_atlas.mk
+# Atlas-related targets
 include make/db_atlas.mk
+# sqlc-related targets
+include make/db_sqlc.mk
