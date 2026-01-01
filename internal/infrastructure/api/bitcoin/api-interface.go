798a6e14d242110eeb47391f70071dea7603d6d7
HirokiYasui <hiromaily2@gmail.com>
2026-01-01 21:39:17 +0900

    tweak

--- internal/infrastructure/api/bitcoin/api-interface.go
+++
@@ -1,5 +0,0 @@
-package bitcoin
-
-// This file contains infrastructure implementations for Bitcoin/BitcoinCash.
-// The Bitcoiner interface definition has been moved to internal/application/ports/btc/interface.go
-// in accordance with Clean Architecture principles.
--- pkg/null/converter.go
+++ pkg/null/converter.go
@@ -1,4 +1,4 @@
-package watch
+package null
 
 import (
 	"database/sql"
